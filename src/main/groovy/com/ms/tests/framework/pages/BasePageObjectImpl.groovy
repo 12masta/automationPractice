06b5d19848f4a9985c31662ec12e16f6beea26d9
12masta <stanek.marcinp@gmail.com>
2017-12-13 22:18:42 +0100

    BasePageObject basic implementation

--- src/main/groovy/com/ms/tests/framework/pages/BasePageObjectImpl.groovy
+++ src/main/groovy/com/ms/tests/framework/pages/BasePageObjectImpl.groovy
@@ -1,4 +1,13 @@
 package com.ms.tests.framework.pages
 
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.PageFactory
+
 class BasePageObjectImpl implements BasePageObject {
+    protected WebDriver driver
+
+    BasePageObjectImpl(WebDriver driver) {
+        this.driver = driver
+        PageFactory.initElements(driver, this)
+    }
 }
